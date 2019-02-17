ef671765d1b6616e63bc9c0d147b48ea1dc26176
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-16 22:21:13 -0200

    Criando Aliases para classe ListaLeitura

---
+++ Alura.WebAPI.WebApp/Api/ListaLeituraController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ListaLeituraController : ControllerBase
+    {
+
+
+
+    }
+}
